017edeb03bfdc22d685aec9b98957d2615e78330
grupoi9tec <lucas.costa@grupoi9tec.com.br>
2017-09-30 20:52:52 -0300

    teste

--- app/src/main/java/br/com/insidegroup/insidecomerce/ActListaTicket.java
+++ app/src/main/java/br/com/insidegroup/insidecomerce/ActListaTicket.java
@@ -129,7 +129,8 @@ public class ActListaTicket extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        int id =
+                item.getItemId();
 
         if (id == R.id.Agenda) {
             // Handle the camera action
