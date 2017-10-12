82d730c0d239ba82c015cda08fb4005a797d7de4
douglas_goncalves_0712 <douglasg.o.costa@gmail.com>
2017-10-12 01:05:53 -0300

    Controle de Erros e MVC no Login

--- app/src/main/java/br/com/insidegroup/insidecomerce/util/EnumMensagemErro.java
+++ app/src/main/java/br/com/insidegroup/insidecomerce/util/EnumMensagemErro.java
@@ -7,7 +7,9 @@ public enum EnumMensagemErro {
     MENSAGEM_ERRO_ACESSO_INTERNET(2, "Não existe conexão com a internet!"),
     MENSAGEM_ERRO_PERMISSAO_INTERNET(3, "Permissão de acesso a internet não concedida"),
     MENSAGEM_ERRO_PREENCHA_TODOS_OS_CAMPOS(18, "Preencha todos os campo"),
-    MENSAGEM_ERRO_OBTER_LISTA_TICKET(19, "Erro ao pegar obter os Tickets");
+    MENSAGEM_ERRO_OBTER_LISTA_TICKET(19, "Erro ao pegar obter os Tickets"),
+    MENSAGEM_ERRO_USUARIO_NAO_INDENTIFICADO(19, "Usuário não identificado."),
+    MENSAGEM_ERRO_AO_IDENTIFICAR_USUARIO(19, "Erro ao indentificar usuario");
 
 
     private int id;
